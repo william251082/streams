79e63a636a578e9d061b2309ce59c2e420aaeb78
william <william.del.rosario@iceshop.nl>
2020-02-09 15:12:45 +0100

    components scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-      <div>
-          PageOne
-          <Link to="/pagetwo">Navigate to page 2</Link>
-      </div>
-  );
-};
-
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo
-            <Link to="/">Navigate to page 1</Link>
-        </div>
-    );
-};
-
+import { BrowserRouter, Route} from "react-router-dom";
+import StreamList from "./components/streams/StreamList";
+import StreamCreate from "./components/streams/StreamCreate";
+import StreamEdit from "./components/streams/StreamEdit";
+import StreamDelete from "./components/streams/StreamDelete";
+import StreamShow from "./components/streams/StreamShow";
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
