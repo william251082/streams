0e2b6f5126dedb8c9f507a643ccaa30c2e74ed37
william <william.del.rosario@iceshop.nl>
2020-02-09 12:38:21 +0100

    how not to handle navigation

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>
+  return (
+      <div>
+          PageOne
+          BAD!
+          <a href="/pagetwo">Navigate to page 2</a>
+      </div>
+  );
 };
 
 const PageTwo = () => {
@@ -16,7 +22,6 @@ const App = () => {
             <BrowserRouter>
                 <div>
                     <Route path="/" exact component={PageOne} />
-                    <Route path="/" component={PageOne} />
                     <Route path="/pagetwo" component={PageTwo} />
                 </div>
             </BrowserRouter>
