dd0c4c75ab748a2a899377acbee9a4f81cb328c4
william <william.del.rosario@iceshop.nl>
2020-02-16 18:34:12 +0100

    linking to stream creation

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {fetchStreams} from "../../actions";
 import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 
 class StreamList extends React.Component {
     componentDidMount() {
@@ -39,6 +40,18 @@ class StreamList extends React.Component {
         })
     }
 
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
@@ -46,6 +59,7 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
         );
     }
@@ -55,6 +69,7 @@ const mapStateToProps = state => {
     return {
         streams: Object.values(state.streams),
         currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
 };
 
