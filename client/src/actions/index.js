65c37e78fec8117cfa47eb719dbd8d03ad5cba0e
william <william.del.rosario@iceshop.nl>
2020-02-16 13:28:17 +0100

    handling fetching, creating and fetching

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -58,34 +58,3 @@ export const deleteStream = id => async dispatch => {
         payload: id
     })
 };
-
-// // array-based approach on updating data
-// const streamReducer = (state=[], action) => {
-//     switch (action.type) {
-//         case EDIT_STREAM:
-//             return state.map(stream => {
-//                 if (stream.id === action.payload.id) {
-//                     return action.payload;
-//                 } else {
-//                     return stream;
-//                 }
-//             });
-//         default:
-//             return state;
-//     }
-// };
-
-// Object-based approach on updating data
-const streamReducer = (state={}, action) => {
-    switch (action.type) {
-        case EDIT_STREAM:
-            // const newState = { ...state };
-            // newState[action.payload.id] = action.payload;
-            // return newState;
-
-            // alternate syntax approach --key interpolation, [action.payload.id]
-            return { ...state, [action.payload.id]: action.payload };
-        default:
-            return state;
-    }
-};
---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,14 @@
+import {CREATE_STREAM, EDIT_STREAM, FETCH_STREAM} from "../actions/types";
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+            return { ...state, [action.payload]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload]: action.payload };
+        case EDIT_STREAM:
+            return { ...state, [action.payload]: action.payload };
+        default:
+            return state;
+    }
+}
