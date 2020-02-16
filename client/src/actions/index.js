65945ab5ad44af8acf2c41b8dd758349614c8161
william <william.del.rosario@iceshop.nl>
2020-02-16 13:13:34 +0100

    object based reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-import {CREATE_STREAM, SIGN_IN, SIGN_OUT} from "./types";
+import {CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM, FETCH_STREAMS, SIGN_IN, SIGN_OUT} from "./types";
 import streams from "../apis/streams";
 
 export const signIn = (userId) => {
@@ -15,10 +15,62 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-    const response = await  streams.post('/streams', formValues);
+    const response = await streams.post('/streams', formValues);
 
     dispatch({
         type: CREATE_STREAM,
         payload: response.data
     })
 };
+
+export const fetchStream = id => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({
+        type: FETCH_STREAM,
+        payload: response.data
+    })
+};
+
+export const fetchStreams = id => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({
+        type: FETCH_STREAMS,
+        payload: response.data
+    })
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({
+        type: EDIT_STREAM,
+        payload: response.data
+    })
+};
+
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({
+        type: DELETE_STREAM,
+        payload: id
+    })
+};
+
+// // array-based approach on updating data
+// const streamReducer = (state=[], action) => {
+//     switch (action.type) {
+//         case EDIT_STREAM:
+//             return state.map(stream => {
+//                 if (stream.id === action.payload.id) {
+//                     return action.payload;
+//                 } else {
+//                     return stream;
+//                 }
+//             });
+//         default:
+//             return state;
+//     }
+// };
