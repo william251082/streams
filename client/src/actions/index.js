50ee9460a69946a94d0d0c199db810c689a7fa7a
william <william.del.rosario@iceshop.nl>
2020-02-16 19:06:13 +0100

    implementing programmatic navigation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -21,8 +21,9 @@ export const createStream = formValues => async (dispatch, getState) => {
     dispatch({
         type: CREATE_STREAM,
         payload: response.data
-    })
+    });
     // do some programmatic navigation to get the user back to the root route
+    history.push('/');
 };
 
 export const fetchStream = id => async dispatch => {
