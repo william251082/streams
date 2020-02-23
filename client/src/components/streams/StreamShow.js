5ed6e0b65a972bde743536c045d0093848ad3a73
william <william.del.rosario@iceshop.nl>
2020-02-23 13:41:42 +0100

    optional player building

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -11,9 +11,22 @@ class StreamShow extends React.Component {
     }
 
     componentDidMount() {
-        const { id } = this.props.match.params;
+        console.log(this.videoRef)
 
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const { id } = this.props.match.params;
         this.player = flv.createPlayer({
             type: 'flv',
             url: `http://localhost:8000/live/${id}.flv`
@@ -21,11 +34,12 @@ class StreamShow extends React.Component {
         this.player.attachMediaElement(this.videoRef.current);
         this.player.load();
     }
+
     render() {
         if (!this.props.stream) {
             return <div>Loading...</div>;
         }
-        const { title, description } = this.props.stream
+        const { title, description } = this.props.stream;
 
         return (
             <div>
