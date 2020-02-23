650d0e4f868e6045d77f14e49e9d3b2b1942ee96
william <william.del.rosario@iceshop.nl>
2020-02-23 12:02:55 +0100

    conditionally showing stream details

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -9,7 +9,7 @@ class StreamDelete extends React.Component {
         this.props.fetchStream(this.props.match.params.id);
     }
 
-    renderActions () {
+    renderActions() {
         return (
             <>
                 <button className="ui button negative">Delete</button>
@@ -18,23 +18,28 @@ class StreamDelete extends React.Component {
         );
     }
 
-    render() {
-        return (
-            <div>
-                StreamDelete
-                <Modal
-                    title="Delete Stream"
-                    content="Are you sure you want to delete this stream?"
-                    actions={this.renderActions()}
-                    onDismiss={() => history.push('/')}
-                />
-            </div>
-        );
+    renderContent() {
+        if (!this.props.streams) {
+            return 'Are you sure you want to delete the stream?'
+        }
+
+        return `Are you sure you want to delete the stream with title: ${this.props.streams.title}`;
     }
 
-    mapStateToProps() {
-
+    render() {
+        return (
+            <Modal
+                title={this.props.streams.title}
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/')}
+            />
+        );
     }
 }
 
-export default connect(null, {fetchStream} ) (StreamDelete);
+const mapStateToProps = (state, ownProps) => {
+    return {streams: state.streams[ownProps.match.params.id]}
+};
+
+export default connect(mapStateToProps, {fetchStream})(StreamDelete);
