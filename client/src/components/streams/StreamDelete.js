c8c319f638b98db1744a7351cf80630e88075d6e
william <william.del.rosario@iceshop.nl>
2020-02-23 10:50:07 +0100

    onDismiss from the parent

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
     const actions = (
-        <React.Fragment>
+        <>
             <button className="ui button negative">Delete</button>
             <button className="ui button">Cancel</button>
-        </React.Fragment>
-        // <div>
-        //     <button className="ui button negative">Delete</button>
-        //     <button className="ui button">Cancel</button>
-        // </div>
+        </>
 
     );
     return (
@@ -20,6 +17,7 @@ const StreamDelete = () => {
                 title="Delete Stream"
                 content="Are you sure you want to delete this stream?"
                 actions={actions}
+                onDismiss={() => history.push('/')}
             />
         </div>
     );
